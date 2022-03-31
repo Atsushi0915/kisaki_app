2a8303f6b9cfec255c20dd913d0e2c7988d07158
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-04-01 07:54:23 +0900

    User Post / model controller 作成

---
+++ db/migrate/20220331223354_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+      t.string :user_id
+      t.string :post_image
+      t.integer :del_flag
+
+      t.timestamps
+    end
+  end
+end
