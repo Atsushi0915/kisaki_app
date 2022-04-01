f38166b336c5a7ea83c67922aa02b97c542d51e6
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-04-01 14:21:12 +0900

    新規ユーザー登録ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_index_path
+    else
+      render :new
+    end
+
+  end
+
   def index
   end
 
@@ -7,4 +21,9 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   root 'posts#top'
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
+
+  get 'user_new',  to:  'users#new',   as:  'user_new'
+  post 'user_create', to:  'users#create',  as:  'user_create'
+  get  'user_index',  to:  'users#index',   as:  'users_index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
