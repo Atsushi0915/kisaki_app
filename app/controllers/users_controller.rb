75df928d632d7c623111bfb7690e1db396f292d3
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-04-01 16:00:09 +0900

    flashメッセージ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash.notice = "新規登録しました"
       redirect_to users_index_path
     else
+      flash.alert = "登録失敗しました!!"
       render :new
     end
 
