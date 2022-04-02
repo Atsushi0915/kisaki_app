e26c091779f39aff128a8c06ac3b0fb9df50d1d8
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-04-03 07:40:57 +0900

    Signin時に session[:user_id]=user.id 追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash.notice = "新規登録しました"
       redirect_to users_index_path
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   root 'posts#top'
 
-  get  'user_new',    to:  'users#new',   as:    'user_new'
-  post 'user_create', to:  'users#create',  as:  'user_create'
-  get  'user_index',  to:  'users#index',   as:  'users_index'
+  get  'user_new',    to:  'users#new',    as:  'user_new'
+  post 'user_create', to:  'users#create', as:  'user_create'
+  get  'user_index',  to:  'users#index',  as:  'users_index'
   
-  get  'login',  to:  'sessions#new',     as:   'login_form'
-  post 'login',  to:   'sessions#create',   as:  'login'
-  get  'logout',  to:  'sessions#destroy',  as:   'logout'
+  get  'login',  to:  'sessions#new',      as:  'login_form'
+  post 'login',  to:  'sessions#create',   as:  'login'
+  get  'logout', to:  'sessions#destroy',  as:  'logout'
+
+
   
   
 end
